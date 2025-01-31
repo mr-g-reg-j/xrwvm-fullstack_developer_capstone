bb832b37588b068f67c52d20bd25eb3acf62b191
mr-g-reg-j <gregworksliaj@gmail.com>
2025-01-31 10:40:24 -0500

    Dealer Page Errors Module 4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,3 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,21 +6,16 @@ from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
 
-    # path for login
     path(route='login', view=views.login_user, name='login'),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path(route='logout',view=views.logout_request, name='logout'),
     path('register/', views.registration, name='registration'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
-    # path for dealer reviews view
-
-    # path for add a review view
-
+  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
