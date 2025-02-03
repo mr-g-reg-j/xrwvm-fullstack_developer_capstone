8e0406cd9f25c1fdd352b3759aac3d8876bf8cdf
mr-g-reg-j <gregworksliaj@gmail.com>
2025-02-03 14:23:46 -0500

    Good up to View Reviews which flashes white

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,15 +7,25 @@ from django.views.generic import TemplateView
 app_name = 'djangoapp'
 urlpatterns = [
 
+    # Path for registration
+    path(route='register', view=views.registration, name='register'),
+
+    # Path for login
     path(route='login', view=views.login_user, name='login'),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path(route='logout',view=views.logout_request, name='logout'),
-    path('register/', view=views.registration, name='registration'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    path(route='logout', view=views.logout_request, name='logout'),
+
+    # Path for dealer reviews view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+
+    # Path for adding a review view
     path(route='add_review', view=views.add_review, name='add_review'),
-  
+
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+  
+    
