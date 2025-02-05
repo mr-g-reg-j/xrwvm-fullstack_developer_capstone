72180e04d706be98093bf478ab78d7341241e9db
mr-g-reg-j <184136261+mr-g-reg-j@users.noreply.github.com>
2025-02-05 20:49:30 +0000

    Continued linting..continued worry

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 
-from django.http import JsonResponse
+
 from django.contrib.auth import login, authenticate
 import logging
 import json
@@ -75,12 +75,14 @@ def registration(request):
 
         login(request, user)
         return (
-            JsonResponse({"status": "success", "userName": username}, status=201)
+            JsonResponse({"status": "success", "userName": username}, 
+            status=201)
         )
 
     return (
              JsonResponse({"error": "Invalid request method"}, status=405)
     )
+
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
@@ -90,12 +92,12 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append(
+            {"CarModel": car_model.name, "CarMake": car_model.car_make.name}
+        )
     return JsonResponse({"CarModels": cars})
 
 
-# # Update the `get_dealerships` view to render the index page with
-# Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if state == "All":
         endpoint = "/fetchDealers"
@@ -132,12 +134,13 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if request.user.is_anonymous == False:
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
             response = post_review(data)
             return JsonResponse({"status": 200})
         except:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({"status": 401, "message": "Error in posting review"}
+            )
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
